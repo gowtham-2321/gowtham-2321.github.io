90ca2bad1fa8905dffbbaa198a599cd0fe1f93c3
Gowtham Jegathesan <83274183+gowtham-2321@users.noreply.github.com>
2024-05-18 15:34:13 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Student","Game Dev","Programmer", "Designer"],
+  strings : ["Student","Game Dev","Coder", "Designer"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
